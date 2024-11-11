723d45589fea876cd36527f71a7dd052f2a15980
aditya-khambekar <adityakhambekar9@gmail.com>
2024-11-11 11:02:51 -0600

    skeleton

--- src/main/java/frc/robot/constants/ShooterSetpoint.java
+++
@@ -1,3 +0,0 @@
-package frc.robot.constants;
-
-public record ShooterSetpoint(double speed, double angle) {}
--- src/main/java/frc/robot/constants/ShooterSetpointMeasurement.java
+++
@@ -1,3 +0,0 @@
-package frc.robot.constants;
-
-public record ShooterSetpointMeasurement(double distance, ShooterSetpoint setpoint) {}
